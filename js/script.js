4c7c0ea81e62581c87974af422eb6ec8319418ed
francesca <fradeluca85@gmail.com>
2021-11-04 16:37:52 +0100

    password

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// alert ("hello world")
+
+
+
+// richiesta nome
+
+const myNome = prompt ("Inserisci il tuo nome");
+// console.log(myNome);
+
+
+// richiesta cognome
+
+const myCongnome = prompt ("Inserisci il tuo cognome");
+// console.log(myCongnome);
+
+
+
+// richiesta cognome
+
+const myColore = prompt ("Inserisci il tuo colore preferito");
+// console.log(myColore);
+
+
+let password = myNome + myCongnome +  "" + myColore + "21";
+document.getElementById("password").innerHTML = password;
+console.log(password);
